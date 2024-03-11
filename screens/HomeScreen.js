e77da5d42d2c820e287088b4868b893ff02b1ff5
kaavlu <manavk@umich.edu>
2024-03-10 23:19:40 -0400

    added home + profile button (need to fix home button to middle)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Linking, Alert, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -17,10 +18,12 @@ const HomeScreen = () => {
     }
   };
 
-  const handleBarPress = () => {
+  const handleBarPressHome = () => {
+    navigation.navigate('Home');
+  };
+  const handleBarPressProfile = () => {
     navigation.navigate('Profile');
   };
-
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Welcome, Will!</Text>
@@ -34,8 +37,12 @@ const HomeScreen = () => {
       </View>
       {/*Add the calendar over here..*/}
       <View style={styles.bottomBar}>
-        <TouchableOpacity style={styles.barButton} onPress={handleBarPress}>
-            <Image source={require('../assets/icon.png')} style={styles.barButtonImage} />
+        <TouchableOpacity style={styles.profileButton} onPress={handleBarPressHome}>
+            {/* <Image source={require('../assets/icon.png')} style={styles.barButtonImage} /> */}
+            <MaterialIcons name="home" size={32} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profileButton} onPress={handleBarPressProfile}>
+          <MaterialIcons name="person" size={32} color="black" />
         </TouchableOpacity>
       </View>
     </View>
@@ -43,64 +50,84 @@ const HomeScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    backgroundColor: '#fc7108',
-    paddingTop: 50,
-  },
-  welcomeText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-    marginBottom: 30,
-    marginLeft: 30,
-    alignSelf: 'flex-start',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around', 
-    alignItems: 'flex-end',
-    paddingHorizontal: 10,
-  },
-  button: {
-    backgroundColor: 'white',
-    paddingVertical: 50,
-    paddingHorizontal: 15,
-    borderRadius: 15,
-    width: '40%',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'left',
-  },
-  bottomBar: {
-    position: 'absolute',
-    bottom: 0,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: 'white',
-    height: 80,
-  },
-  barButton: {
-    width: 100,
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderRadius: 50,
-    bottom: 20,
-  },
-  barButtonImage: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-  }
-});
+    container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      backgroundColor: '#fc7108',
+      paddingTop: 50,
+    },
+    welcomeText: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+      marginBottom: 30,
+      marginLeft: 30,
+      alignSelf: 'flex-start',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around', 
+      alignItems: 'flex-end',
+      paddingHorizontal: 10,
+    },
+    button: {
+      backgroundColor: 'white',
+      paddingVertical: 50,
+      paddingHorizontal: 15,
+      borderRadius: 15,
+      width: '40%',
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 2 },
+    },
+    buttonText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlign: 'left',
+    },
+    bottomBar: {
+        position: 'absolute',
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: 'white',
+        height: 80,
+        paddingHorizontal: 10,
+    },
+    barButton: {
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 50,
+        bottom: 20,
+        alignSelf: 'center',
+        position: 'relative',
+    },
+    barButtonImage: {
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+    },
+    profileButtonContainer: {
+      width: 60,
+      height: 60,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      borderRadius: 30,
+    },
+    profileButton: {
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 25,
+        marginRight: 10, // ensure the profile button is pushed to the right
+    },
+  });  
 
 export default HomeScreen;
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Linking, Alert, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const ProfileScreen = ({ navigation }) => {
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const handleBarPressHome = () => {
+    navigation.navigate('Home');
+  };
+  const handleBarPressProfile = () => {
+    navigation.navigate('Profile');
+  };
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Text>Go back to Home</Text>
-      </TouchableOpacity>
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.profileButton} onPress={handleBarPressHome}>
+            {/* <Image source={require('../assets/icon.png')} style={styles.barButtonImage} /> */}
+            <MaterialIcons name="home" size={32} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profileButton} onPress={handleBarPressProfile}>
+          <MaterialIcons name="person" size={32} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#fc7108',
-    alignItems: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      backgroundColor: '#fc7108',
+      paddingTop: 50,
+    },
+    bottomBar: {
+        position: 'absolute',
+        bottom: 0,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: 'white',
+        height: 80,
+        paddingHorizontal: 10,
+    },
+    barButton: {
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 50,
+        bottom: 20,
+        alignSelf: 'center',
+        position: 'relative',
+    },
+    barButtonImage: {
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+    },
+    profileButtonContainer: {
+      width: 60,
+      height: 60,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      borderRadius: 30,
+    },
+    profileButton: {
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 25,
+        marginRight: 10, // ensure the profile button is pushed to the right
+    },
+  });  
 
-export default ProfileScreen;
+export default HomeScreen;
